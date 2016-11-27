cb8b6eb041625e22609b0720d8093bda772577a4
can lekili <clekili@gmail.com>
2016-11-26 22:24:24 -0800

    typofix on FluidBoxRenderer

--- fluidBoxRenderer.js
+++ fluidBoxRenderer.js
@@ -1,9 +1,9 @@
 class FluidBoxRenderer {
-  constructor(canvas, box){
-    this.box = box;
+  constructor(canvas, solver){
+    this.solver = solver;
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
-    this.imageData = this.ctx.createImageData(box.width(), box.height());
+    this.imageData = this.ctx.createImageData(solver.width(), solver.height());
 
     // set image data to alpha to 255
     for(let i = 3; i < this.imageData.data.length; i += 4)
@@ -12,18 +12,18 @@ class FluidBoxRenderer {
 
   render(){
     let data = this.imageData.data;
-    let height = this.box.height();
-    let width = this.box.width();
+    let height = this.solver.height();
+    let width = this.solver.width();
 
     for(let i = 0; i < width; i++){
       for(let j = 0; j < height; j++){
         let pixel = 4 * (j * height + i);
         // red
-        data[pixel + 0] = this.box.getDensity(i, j) * 255 * i / width / 5;
+        data[pixel + 0] = this.solver.getDensity(i, j) * 255 * i / width / 5;
         // green
-        data[pixel + 1] = this.box.getDensity(i, j) * 255 * j / width / 5;
+        data[pixel + 1] = this.solver.getDensity(i, j) * 255 * j / width / 5;
         // blue
-        data[pixel + 2] = this.box.getDensity(i, j) * 255 / 5;
+        data[pixel + 2] = this.solver.getDensity(i, j) * 255 / 5;
       }
     }
 
