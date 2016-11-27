f35ff7a95ad1e1a178374883d30560560e17f2b8
can lekili <clekili@gmail.com>
2016-11-26 22:25:00 -0800

    add styling and title

--- entry.js
+++ entry.js
@@ -1,8 +1,8 @@
 import FluidSimulator from './fluidSimulator.js';
 
 document.addEventListener('DOMContentLoaded', function(){
-  let width = 900;
-  let height = 500;
+  let width = document.getElementById('fluidSimulator').offsetWidth;
+  let height = document.getElementById('fluidSimulator').offsetHeight;
   let resolution = 200;
   let density = 90;
   let simulator = new FluidSimulator(width, height, resolution, density);
